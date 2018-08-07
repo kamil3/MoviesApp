c3047544b0301e361df4766b585a40d0522d207c
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-07 14:08:13 +0200

    Create PopularMoviesViewController and it's storyboard

---
+++ MoviesApp/TabBar/PopularMovies/PopularMoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PopularMoviesViewController.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 07.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+import UIKit
+
+class PopularMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
