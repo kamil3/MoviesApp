5ac9b82de520071c1b51b1f3ce7734d8c4ccccd0
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-07 14:05:09 +0200

    Create TopRatedMoviesViewController and it's storyboard

---
+++ MoviesApp/TabBar/TopRatedMovies/TopRatedMoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopRatedMoviesViewController.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 07.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
