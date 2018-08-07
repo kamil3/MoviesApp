065eb06221fd9918d016a6633ed7c444261c3c07
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-07 13:35:05 +0200

    Remove unnecessary default files and create MainTabBarController view

---
+++ MoviesApp/TabBar/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 07.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
