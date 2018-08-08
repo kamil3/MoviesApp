2cbc5f0657c1600db0176761a77f6bb096d77df7
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-08 10:06:41 +0200

    Create adequate view models

---
+++ MoviesApp/TabBar/PopularMovies/PopularMoviesViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  PopularMoviesViewModel.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 08.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+struct PopularMoviesViewModel {
+    
+}
---
+++ MoviesApp/TabBar/TopRatedMovies/TopRatedMoviesViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  TopRatedMoviesViewModel.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 08.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+struct TopRatedMoviesViewModel {
+    
+}
