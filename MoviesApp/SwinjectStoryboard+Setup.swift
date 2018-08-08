b74c1d325c58f590f8c5e617238babd847376834
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-08 12:15:52 +0200

    Register basic components in SwinjectStoryboard+Setup

--- MoviesApp/SwinjectStoryboard+Setup.swift
+++ MoviesApp/SwinjectStoryboard+Setup.swift
@@ -8,16 +8,37 @@
 
 import Swinject
 import SwinjectStoryboard
+import Alamofire
 
 extension SwinjectStoryboard {
     @objc class func setup() {
         Container.loggingFunction = nil
+        
+        //APP DEPENDENCIES CONTAINER
+        defaultContainer.register(AppDependency.self) { r in
+            return AppDependency(movieService: r.resolve(MovieServiceProtocol.self)!)
+        }
         
         //CORE COMPONENTS
         
         /* AppConfig */
         defaultContainer.register(AppConfig.self) { r in
             return AppConfig()
+        }
+        .inObjectScope(.container)
+        
+        /* ApiClient */
+        defaultContainer.register(ApiClientProtocol.self) { r in
+            let configuration = URLSessionConfiguration.default
+            configuration.timeoutIntervalForRequest = 20
+            let sessionManager = SessionManager(configuration: configuration)
+            return ApiClient(sessionManager: sessionManager)
+        }
+        .inObjectScope(.container)
+        
+        /* MovieService */
+        defaultContainer.register(MovieServiceProtocol.self) { r in
+            return MovieService(with: r.resolve(ApiClientProtocol.self)!)
         }
         .inObjectScope(.container)
     }
