cd3928a0b277b98c157cc9810702b37332a5952c
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-10 10:10:55 +0200

    Create AlternativeTitles view with related files

---
+++ MoviesApp/TabBar/PopularMovies/AlternativeTitles/AlternativeTitlesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AlternativeTitlesViewController.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 10.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+import UIKit
+
+class AlternativeTitlesViewController: UIViewController {
+
+    // MARK:- Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ MoviesApp/TabBar/PopularMovies/AlternativeTitles/AlternativeTitlesViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  AlternativeTitlesViewModel.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 10.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+struct AlternativeTitlesViewModel {
+    
+}
