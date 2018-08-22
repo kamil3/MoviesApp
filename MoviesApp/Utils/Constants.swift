726189d312729c44ece3d331c099154a0c3960ac
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-22 21:28:37 +0200

    Create Constants.swift

---
+++ MoviesApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 22.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+struct ImageSizeConstants {
+    static let TopRatedSize = "w185"
+}
