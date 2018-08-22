889843c23b7069369e85cf3d473c507f15bd2cc4
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-22 14:52:55 +0200

    Create TopRatedMovieCollectionViewCell

---
+++ MoviesApp/TabBar/TopRatedMovies/CollectionViewCell/TopRatedMovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TopRatedMovieCollectionViewCell.swift
+//  MoviesApp
+//
+//  Created by Kamil Waszkiewicz on 22.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedMovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
