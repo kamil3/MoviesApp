15bd0109b1ba003dc38e9d9b8f2ff07fe0077659
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-22 21:51:51 +0200

    Implement TopRatedMovieCollectionViewCell using images and labels

--- MoviesApp/TabBar/TopRatedMovies/CollectionViewCell/TopRatedMovieCollectionViewCell.swift
+++ MoviesApp/TabBar/TopRatedMovies/CollectionViewCell/TopRatedMovieCollectionViewCell.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Kingfisher
+import SwinjectStoryboard
 
 class TopRatedMovieCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var voteCountNamingLabel: UILabel!
+    @IBOutlet weak var voteCountLabel: UILabel!
+    @IBOutlet weak var voteAverageNamingLabel: UILabel!
+    @IBOutlet weak var voteAverageLabel: UILabel!
+    @IBOutlet weak var infoButton: UIButton!
+    
+    static let imageURLString = SwinjectStoryboard.defaultContainer.resolve(AppConfig.self)!.imagesURL
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        containerView.layer.cornerRadius = 5
+        containerView.layer.masksToBounds = true
     }
-
+    
+    func update(with movie: Movie) {
+        let imageUrl: URL? = (movie.posterPath != nil) ? URL(string: "\(TopRatedMovieCollectionViewCell.imageURLString)\(ImageSizeConstants.TopRatedSize)/\(movie.posterPath!)") : nil
+        imageView.kf.setImage(with: imageUrl)
+        movieTitleLabel.text = movie.title
+        voteCountNamingLabel.text = "top.rated.movies.cell.vote.count.naming.label".localized()
+        voteCountLabel.text = movie.voteCount.asStringOrEmpty()
+        voteAverageNamingLabel.text = "top.rated.movies.cell.vote.average.naming.label".localized()
+        voteAverageLabel.text = movie.voteAverage.asStringOrEmpty()
+    }
 }
 
 extension TopRatedMovieCollectionViewCell: NibLoadableView {}
