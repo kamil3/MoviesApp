f16b798261f3ff04373cff12b96e2b4e9aea3484
Kamil Waszkiewicz <kamil.waszkiewicz@hotmail.com>
2018-08-27 10:45:26 +0200

    Create MovieServiceMock and implement it with dummy return values

---
+++ MoviesAppTests/Mocks/MovieServiceMock.swift
@@ -0,0 +1,27 @@
+//
+//  MovieServiceMock.swift
+//  MoviesAppTests
+//
+//  Created by Kamil Waszkiewicz on 27.08.2018.
+//  Copyright © 2018 kamil. All rights reserved.
+//
+
+@testable import MoviesApp
+import RxSwift
+
+struct MovieServiceMock: MovieServiceProtocol {
+    var moviesReturnValue: Observable<[Movie]> = .empty()
+    var alternativeMovieTitlesReturnValue: Observable<AlternativeMovieTitle> = .empty()
+    
+    func topRatedMovies() -> Observable<[Movie]> {
+        return moviesReturnValue
+    }
+    
+    func popularMovies() -> Observable<[Movie]> {
+        return moviesReturnValue
+    }
+    
+    func alternativeMovieTitles(withMovieId movieId: String) -> Observable<AlternativeMovieTitle> {
+        return alternativeMovieTitlesReturnValue
+    }
+}
